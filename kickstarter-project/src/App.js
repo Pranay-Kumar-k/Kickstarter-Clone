9549252c84fd1005e770a3fd2905d1f673a89f77
Pranay Kumar <pranaykumar31may1996@gmail.com>
2020-12-30 18:23:09 +0530

    merging with main

--- kickstarter-project/src/App.js
+++ kickstarter-project/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import StartAProject from "./Components/Pages/StartAProject"
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 require('react-dom');
 window.React2 = require('react');
 console.log(window.React1 === window.React2);
