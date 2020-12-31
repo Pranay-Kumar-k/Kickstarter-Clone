e2fc66faa5e912ed512d7dcb8ece645a615c98fa
Pranay Kumar <pranaykumar31may1996@gmail.com>
2020-12-31 11:58:26 +0530

    Designed Page 3

--- kickstarter-project/src/App.js
+++ kickstarter-project/src/App.js
@@ -4,19 +4,18 @@ import { Header } from "./Components/Header";
 import Routes from "./Components/Routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Page2 } from "./Components/Pages/StartPage2";
-import { Page3 } from "./Components/Pages/StartPage3";
+// import { Page3 } from "./Components/Pages/StartPage3";
 
 require('react-dom');
 window.React2 = require('react');
 console.log(window.React1 === window.React2);
-// import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       {/* <Header />
       <Routes /> */}
-      <Page3 />
+      {/* <Page3 /> */}
     </div>
   );
 }
